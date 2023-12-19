b70a9406d0430922320f342fba5a001db07d0824
coskudemirhan <cosku@dokuziki.com>
2023-12-19 15:10:48 +0300

    video updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import styles from './pageStyles.module.css';
 const Home = () => {
   return (
     <div className={styles.wrapper}>
-      <FullscreenVideo videoSrc="/video.mp4" />
+      <FullscreenVideo videoSrc="/video.webm" />
     </div>
   );
 };
